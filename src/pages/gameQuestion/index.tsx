13e7c1de8bf5280d678545b60da0f2b0cc63f0bd
na_0_i <nayoi1oo5@gmail.com>
2023-07-19 22:19:41 +0900

    [feat] GameQuestion 페이지 구현

--- src/pages/gameQuestion/index.tsx
+++ src/pages/gameQuestion/index.tsx
@@ -1,5 +1,112 @@
+import { getGameInfo } from "@/api/gameAPI";
+import { QuestionType } from "@/interface/gameInterface";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+import LogoIcon from "@/assets/logo.png";
+import { ReactComponent as BackIcon } from "@/assets/back.svg";
+import { ReactComponent as ForwardIcon } from "@/assets/forward.svg";
+
 const GameQuestion = () => {
-    return <div>GameQuestion</div>;
+    const navigate = useNavigate();
+    const location = useLocation();
+    const gameId = location.pathname.split("/").at(-1);
+    const [gameQuestions, setGameQuestions] = useState<QuestionType[]>([]);
+    const [curIdx, setCurIdx] = useState(0);
+
+    const fetchGameInfo = async (gameId: string) => {
+        const data = await getGameInfo(gameId);
+        if (data) setGameQuestions(data.questions);
+    };
+
+    const handleClickBackButton = () => {
+        if (curIdx > 0) setCurIdx(curIdx - 1);
+        else alert("게임을 나가시겠습니까?");
+    };
+
+    const handleClickForwardButton = () => {
+        if (curIdx < gameQuestions.length - 1) setCurIdx(curIdx + 1);
+        else navigate("/");
+    };
+
+    useEffect(() => {
+        if (gameId) fetchGameInfo(gameId);
+    }, [gameId]);
+
+    return (
+        <GameQuestionContainer>
+            <GameQuestionHeader>
+                <Logo src={LogoIcon} />
+                <QuestionIndex>{curIdx + 1}번째 질문</QuestionIndex>
+            </GameQuestionHeader>
+
+            <Gradient>
+                <Question>{gameQuestions[curIdx]?.content}</Question>
+            </Gradient>
+
+            <ButtonsSection>
+                <MoveButton onClick={handleClickBackButton}>
+                    <BackIcon />
+                </MoveButton>
+                <MoveButton onClick={handleClickForwardButton}>
+                    <ForwardIcon />
+                </MoveButton>
+            </ButtonsSection>
+        </GameQuestionContainer>
+    );
 };
 
 export default GameQuestion;
+
+const GameQuestionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+`;
+
+const GameQuestionHeader = styled.header`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    padding-top: 20px;
+`;
+
+const Logo = styled.img`
+    width: 30px;
+    height: 30px;
+`;
+
+const QuestionIndex = styled.span``;
+
+const Question = styled.p``;
+
+const Gradient = styled.div`
+    width: 100%;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: radial-gradient(
+        circle closest-side at 50% 50%,
+        #06ad5430,
+        white
+    );
+`;
+
+const ButtonsSection = styled.div`
+    display: flex;
+    gap: 50px;
+    padding-bottom: 40px;
+`;
+
+const MoveButton = styled.button`
+    width: 50px;
+    height: 50px;
+    background-color: var(--color-primary-light);
+    border: none;
+    border-radius: 50%;
+`;
